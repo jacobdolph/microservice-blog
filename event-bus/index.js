2fd89fb2728176f6d5c079dadef09a4fb9983a8a
Jacob Dolph <jdolph2@instructors.2u.com>
2022-04-22 17:50:23 -0600

    building out config for cluster ips for services

--- event-bus/index.js
+++ event-bus/index.js
@@ -17,13 +17,13 @@ app.post("/events", async (req, res) => {
     .post("http://posts-clusterip-srv:4000/events", event)
     .catch((err) => console.log(err));
   await axios
-    .post("http://localhost:4001/events", event)
+    .post("http://comments-srv:4001/events", event)
     .catch((err) => console.log(err));
   await axios
-    .post("http://localhost:4002/events", event)
+    .post("http://query-srv:4002/events", event)
     .catch((err) => console.log(err));
   await axios
-    .post("http://localhost:4003/events", event)
+    .post("http://moderation-srv:4003/events", event)
     .catch((err) => console.log(err));
 
   res.send({ status: "OK" });
